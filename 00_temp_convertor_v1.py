1dec72bdc33afd5cc37d31b4e62e62c96e4b3fa3
VYnZerRa <127251601+VYnZerRa@users.noreply.github.com>
2024-02-22 13:15:58 +1300

    Update 00_temp_convertor_v1.py

---
+++ 00_temp_convertor_v1.py
@@ -0,0 +1,59 @@
+from tkinter import *
+
+
+class Converter:
+
+    def __init__(self):
+
+        # Initialise variables (such as the feedback variable)
+        self.var_feedback = StringVar()
+        self.var_feedback.set("")
+
+        self.var_has_error = StringVar()
+        self.var_has_error.set("no")
+
+        self.all_calculations = []
+
+        # common format for all buttons
+        # Arial size 14 bold, with white text
+        button_font = ("Arial", "12", "bold")
+        button_fg = "#FFFFFF"
+
+        
+        #Set up GUI Frame
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Convertor"
+                                  font=("Arial", "16", "bold")
+                                  )
+        self.temp_heading.grid(row=0)
+
+        instructions = "Please enter a temperature below and"
+                        "then press one of the buttons to covert"
+                        "it from centigrade to Fahrenheit."
+        self.temp_intructions = Label(self.temp_frame,
+                                      text=instructions,
+                                      wrap=250, width=40,
+                                      justify="left")
+        self.temp_intructions.grid(row=1)
+
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Arial", "14")
+                                )
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "Please enter a number"
+        self.output_label=Label(self.temp_frame, text="",
+                                fg="#9C0000")
+        self.output_label.grid(row=3)
+
+        # Conversion, help and history / export buttons
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        self.to_celsius_button = Button(self.button_frame,
+                                        text="To Celsius",
+                                        bg="#990099",
+                                        fg=button_fg,
